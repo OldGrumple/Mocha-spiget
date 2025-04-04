41525701e473307138419ebc92d57945651bdcc4
Ben Lack <benlack@Bens-MacBook-Air.local>
2025-04-05 00:26:45 +0100

    Configure GitHub Pages deployment

--- example/vite.config.js
+++ example/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mocha-spiget/demo',  // Replace 'example' with your repository name
+  base: '/Mocha-spiget/example/',
 }) 
